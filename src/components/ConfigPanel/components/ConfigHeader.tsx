178561084579fcce75f25c07a3a62baee1b1521f
BraianAzcune-andreani <bazcune@andreani.com>
2026-04-03 20:06:18 -0300

    feat: quitar dummy

--- src/components/ConfigPanel/components/ConfigHeader.tsx
+++ src/components/ConfigPanel/components/ConfigHeader.tsx
@@ -2,7 +2,7 @@ export function ConfigHeader() {
 	return (
 		<header class="mb-4 border-b border-gray-700 pb-3">
 			<h2 class="text-xl font-semibold text-gray-100">Configuracion</h2>
-			<p class="mt-1 text-sm text-gray-400">lista dummy</p>
+			<p class="mt-1 text-sm text-gray-400">lista</p>
 		</header>
 	);
 }
